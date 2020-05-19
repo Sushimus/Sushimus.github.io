a0a3a1861be2928b3d734d8fb23c0e2bb5ec1ca4
Sushimus <59678594+Sushimus@users.noreply.github.com>
2020-05-19 10:53:11 -0500

    fixed font, edit review, add games

--- reviewGen.js
+++ reviewGen.js
@@ -1,9 +1,18 @@
 var quotes = [
-	'Chex Cinnamon',
-	'Fruity Pebbles',
-	'Cinnamon Toast Crunch',
-	'Fruity Dino Bites',
-	'Special K Red Berry'
+	"Chex Cinnamon!"+"<br>"+
+	"The rest of this line is a test to see how long the words will go before starting a new line I'm trying to make it so that paragraphs work please work paragraphs",
+	
+	"Fruity Pebbles"+"<br>"+
+	"The rest of this line is a test to see how long the words will go before starting a new line I'm trying to make it so that paragraphs work please work paragraphs",
+	
+	"Cinnamon Toast Crunch"+"<br>"+
+	"The rest of this line is a test to see how long the words will go before starting a new line I'm trying to make it so that paragraphs work please work paragraphs",
+	
+	"Fruity Dino Bites"+"<br>"+
+	"The rest of this line is a test to see how long the words will go before starting a new line I'm trying to make it so that paragraphs work please work paragraphs",
+	
+	"Special K Red Berry"+"<br>"+
+	"The rest of this line is a test to see how long the words will go before starting a new line I'm trying to make it so that paragraphs work please work paragraphs"
 ]
 
 function getReview() {
